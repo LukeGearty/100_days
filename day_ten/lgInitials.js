3a9d1f04fb5574dcd7544984014ba9b2228e145e
LukeGearty <119687030+LukeGearty@users.noreply.github.com>
2024-01-20 22:17:45 -0500

    added HTML and JavaScript files for day ten

---
+++ day_ten/lgInitials.js
@@ -0,0 +1,7 @@
+console.log(" ___      _______ ");
+console.log("|   |    |       |");
+console.log("|   |    |    ___|");
+console.log("|   |    |   | __ ");
+console.log("|   |___ |   ||  |");
+console.log("|       ||   |_| |");
+console.log("|_______||_______|");
