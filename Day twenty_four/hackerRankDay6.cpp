c5b754abd3676dee9513ba79d975440247b36022
LukeGearty <119687030+LukeGearty@users.noreply.github.com>
2024-03-10 20:20:14 -0400

    Day twenty four

---
+++ Day twenty_four/hackerRankDay6.cpp
@@ -0,0 +1,32 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    
+    int length;
+    cin >> length;
+    
+    
+    while(length > 0) {
+        string s;
+        cin >> s;
+        for (int i = 0; i < s.length(); i += 2) {
+            cout << s[i];
+        }
+        cout << " ";
+        for (int i = 1; i < s.length(); i += 2) {
+            cout << s[i];
+        }
+        length--;
+        cout << endl;
+    }
+    
+
+    return 0;
+}
